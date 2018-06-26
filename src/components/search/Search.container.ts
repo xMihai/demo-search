2c0907eb11001ae0f7698ec794f36bf88eb3eb9a
Mihai Răducanu <raducanu@gmail.com>
2018-06-26 17:13:09 +0300

    refactor(Search): add input debounce

--- src/components/search/Search.container.ts
+++ src/components/search/Search.container.ts
@@ -3,10 +3,12 @@ import { connect } from 'react-redux'
 import mapDispatchToProps from '@@store/mapDispatchToProps'
 import actions from '@@store/actions'
 import Search from './Search.view'
+import debounceHandler from '@hocs/debounce-handler'
 
 export default compose(
   connect(
     null,
     mapDispatchToProps({ filter: actions.tours.filter })
-  )
+  ),
+  debounceHandler('filter', 500)
 )(Search)
