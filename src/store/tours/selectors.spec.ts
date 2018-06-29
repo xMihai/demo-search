0ccb89c26a6ad4d587c4a06d4e4df76ce20bd7a6
Mihai Răducanu <raducanu@gmail.com>
2018-06-29 14:01:38 +0300

    Updated tests

--- src/store/tours/selectors.spec.ts
+++ src/store/tours/selectors.spec.ts
@@ -1,7 +1,15 @@
 // tslint:disable:no-expression-statement
 import { initialState } from '@@store/reducer'
+import { SORTING } from './state'
 import update from 'immutability-helper'
-import { getData, getFilter, getMatchString, getDataWithMatcher, getFilterKeywords } from './selectors'
+import {
+  getData,
+  getFilter,
+  getMatchString,
+  getDataWithMatcher,
+  getFilterKeywords,
+  getMatchingSortedList,
+} from './selectors'
 import tours from './__mocks__/tours.json'
 
 describe('Tours selectors', () => {
@@ -42,5 +50,27 @@ describe('Tours selectors', () => {
   test('getFilterKeywords > some long keywords', () => {
     const state = update(initialState, { tours: { filter: { $set: 'aa BB CCC' } } })
     expect(getFilterKeywords(state)).toEqual(['aa bb ccc', 'ccc'])
+  })
+
+  test('getMatchingSortedList > price sorting', () => {
+    const state1 = update(initialState, {
+      tours: { data: { $set: tours.slice(0, 10) }, sorting: { $set: SORTING.LOW_PRICE } },
+    })
+    const list1 = getMatchingSortedList(state1).map(tour => tour.length)
+    const state2 = update(state1, { tours: { sorting: { $set: SORTING.HIGH_PRICE } } })
+    const list2 = getMatchingSortedList(state2).map(tour => tour.length)
+    list2.reverse()
+    expect(list1).toEqual(list2)
+  })
+
+  test('getMatchingSortedList > duration sorting', () => {
+    const state1 = update(initialState, {
+      tours: { data: { $set: tours.slice(0, 10) }, sorting: { $set: SORTING.LONGEST } },
+    })
+    const list1 = getMatchingSortedList(state1).map(tour => tour.length)
+    const state2 = update(state1, { tours: { sorting: { $set: SORTING.SHORTEST } } })
+    const list2 = getMatchingSortedList(state2).map(tour => tour.length)
+    list2.reverse()
+    expect(list1).toEqual(list2)
   })
 })
